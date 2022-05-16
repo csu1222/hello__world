19bb49c024a37164aa3ebfd32a161faedb328c34
Seonguk Cheon <cjstjddnr12@gmail.com>
2022-05-16 16:40:25 +0900

    2022-05-16 APP 글 생성 UI

--- WEBnTraining/newnode/main.js
+++ WEBnTraining/newnode/main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const path = require('path');
 
 function templateHTML(title, list, body){
   return`
@@ -13,6 +14,7 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
   </body>
   </html>`;
@@ -32,7 +34,6 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-
 
     if (pathname === '/'){
       if (queryData.id === undefined){
@@ -61,10 +62,32 @@ var app = http.createServer(function(request,response){
           })
       });
       }
+    }  else if (pathname === '/create') {
+      fs.readdir('./data', function(error, filelist){
+        var title = 'WEB - CREATE';
+        var list = templateList(filelist)
+        var template = templateHTML(title, list, 
+        `
+        <form action="http://localhost:3000/process_create" method="post">
+          <p>
+              <input type="text" name="title" placeholder="title">
+          </p>
+          <p>
+              <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+              <input type="submit" value="create">
+          </p>
+        </form>
+        `);
+    
+        response.writeHead(200);
+        response.end(template);
+      });
     } else {
       response.writeHead(404);
       response.end('NOT FOUND');
-    }
+    };
  
 });
 app.listen(3000);
