ac1b3ba826d0e1be5bbb59ceeed7f783e3984c9f
Seonguk Cheon <cjstjddnr12@gmail.com>
2022-05-19 19:05:35 +0900

    2022-05-19 APP delete기능 구현

--- WEBnTraining/newnode/main.js
+++ WEBnTraining/newnode/main.js
@@ -55,7 +55,12 @@ var app = http.createServer(function(request,response){
             var list = templateList(filelist)
             var template = templateHTML(title, list, 
               `<h2>${title}</h2><p>${description}</p>`,
-              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+              `<a href="/create">create</a>
+               <a href="/update?id=${title}">update</a>
+               <form action="delete_process" method="post">
+                <input type="hidden" name="id" value="${title}">
+                <input type="submit" value="delete">
+               </form>`);
         
             response.writeHead(200);
             response.end(template);
@@ -145,6 +150,22 @@ var app = http.createServer(function(request,response){
         /*var title = new URLSearchParams(body).get('title');
         var description = new URLSearchParams(body).get('description');*/
         
+      });
+
+    } else if (pathname === '/delete_process'){
+      var body = '';
+      request.on('data', function(data){
+        body += data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        /*var title = new URLSearchParams(body).get('title');
+        var description = new URLSearchParams(body).get('description');*/
+        fs.unlink(`data/${id}`, function(err){
+          response.writeHead(302, {location : `/`});
+          response.end();
+        })
       });
 
     } else {
